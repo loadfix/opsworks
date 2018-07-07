8462b8c7d1b9f4b331ae6630e89795c3531cfd8c
Ben Hooper <ben@networkinsanity.com>
2018-07-07 23:18:28 +0800

    Custom recipe

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -1,15 +1,11 @@
-include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
 
-  opsworks_deploy do
-    deploy_data deploy
-    app application
-  end
-
+    ruby_block "install_wordpress" do
+        block do
+            require 'fileutils'
+            FileUtils.cd "#{deploy[:deploy_to]}/current/"
+            system 'wget https://wordpress.org/latest.tar.gz'
+            system 'tar -xzf latest.tar.gz --strip-components=1 && rm latest.tar.gz'
+    end
 end
